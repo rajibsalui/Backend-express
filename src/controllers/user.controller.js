a9ac3e7a631c311b285837bf258acfed4881e87a
Rajib Salui <rajibsalui@gmail.com>
2024-03-24 09:47:48 +0530

    initial

---
+++ src/controllers/user.controller.js
@@ -0,0 +1,9 @@
+import {asyncHandler, aysncHandler} from "../utils/asyncHandler.js"
+
+const registerUser=asyncHandler(async (req,res)=>{
+    res.status(200).json({
+        message: "ok"
+    })
+})
+
+export {registerUser}
